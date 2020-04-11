056d8df7927e942b7448dc6c26f85ee91f7b84f7
bin0x00 <sdexca@icloud.com>
2020-04-11 22:02:30 +0000

    Added more functions to make code look more clean and non-repeative

--- lib/__init__.py
+++ lib/__init__.py
@@ -35,6 +35,61 @@ def getTableList(soup_object:object):
         resp.append(resp_add_dict)
     return titles, resp
 
+def getResponceFromRawUrl(raw_url:str):
+    # using requests instent of client.get_memento(raw_url) as its buggy in the time of the creation of this code
+    x = True
+    c = 0
+    while x:  # loop to keep trying...
+        try:
+            c+=1
+            response = requests.get(raw_url, headers={
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
+                "Host": "web.archive.org"
+            })
+            if response != None:
+                x = False
+        except:
+            print("[!] Failed to get the responce of `raw_url`, tring again.")
+            print(f"[!] Count: {c}")
+            pass
+    return response
+
+    # # using client.get_memento with a loop doesnt help, but you could try this when its fixed
+    # x = True
+    # while x:
+    #     try:
+    #         response = client.get_memento(raw_url)
+    #         if response != None:
+    #             x = False
+    #     except:
+    #         print(raw_url)
+    #         pass
+    # return response
+
+def getCheckDatabaseError(raw_html:str, raw_url:str):
+    check_for_server_error = raw_html.find('Database connection failed: Too many connections (1040)')
+    if check_for_server_error >= 0:
+        with open('scrap_error.txt', '+a') as f:
+            f.write(f"[!] couldent fetch from {raw_url} because of database error 1040(too many connections)")
+            print("[!] 1040 Database Error")
+            print("[!] Exiting without Scraping this `raw_url`")
+        exit(0)
+    else: print("[!] No errors found")
+
+def getCheckSnapshot(snapshot_time, all_timestamps):
+    if snapshot_time == 'last-snapshot':
+        last_timestamp_objects = getLastTimeStamps(all_timestamps)
+        for i in last_timestamp_objects:
+            if i.timestamp.strftime('%d-%m-%Y') == date:
+                raw_url = i.raw_url
+                view_url = i.view_url
+    elif snapshot_time == 'first-snapshot':
+        first_timestamp_objects = getFirstTimeStamps(all_timestamps)
+        for i in first_timestamp_objects:
+            if i.timestamp.strftime('%d-%m-%Y') == date:
+                raw_url = i.raw_url
+                view_url = i.view_url
+    return raw_url, view_url
 
 def getLastTimeStamps(timestamps:list):
     last_timestamp_objects = []
@@ -50,6 +105,21 @@ def getLastTimeStamps(timestamps:list):
     timestamps.reverse()
     last_timestamp_objects.append(timestamps[0])
     return last_timestamp_objects
+
+
+def getFirstTimeStamps(timestamps:list):
+    last_timestamp_objects = []
+
+    timestamps.sort()
+    last_timestamp_objects.append(timestamps[0])
+    for i in range(0, len(timestamps)-1):
+        today = timestamps[i].timestamp.strftime('%d-%m-%Y')
+        tomorrow = timestamps[i+1].timestamp.strftime('%d-%m-%Y')
+        if today == tomorrow:
+            pass
+        else:
+            last_timestamp_objects.append(timestamps[i+1])
+    return last_timestamp_objects 
 
 
 def setJsonFile(name:str, json_data:dict):
